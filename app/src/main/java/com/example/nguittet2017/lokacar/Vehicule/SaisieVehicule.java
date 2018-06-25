26fc98e91f747fc684a51902906196637b95f015
Unknown <ducatnico@gmail.com>
2018-06-25 10:27:59 +0200

    Création vehicule

---
+++ app/src/main/java/com/example/nguittet2017/lokacar/Vehicule/SaisieVehicule.java
@@ -0,0 +1,18 @@
+package com.example.nguittet2017.lokacar.Vehicule;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.nguittet2017.lokacar.R;
+
+public class SaisieVehicule extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saisie_vehicule);
+    }
+
+
+    
+}
---
+++ app/src/main/java/com/example/nguittet2017/lokacar/Vehicule/Vehicule.java
@@ -0,0 +1,72 @@
+package com.example.nguittet2017.lokacar.Vehicule;
+
+import android.net.Uri;
+
+public class Vehicule {
+
+    private String immatriculation;
+    private String marque;
+    private String modele;
+    private double prix;
+    private Uri photo;
+    private boolean location;
+
+    public Vehicule(String immatriculation, String marque, String modele, double prix, Uri photo, boolean location) {
+        this.immatriculation = immatriculation;
+        this.marque = marque;
+        this.modele = modele;
+        this.prix = prix;
+        this.photo = photo;
+        this.location = location;
+    }
+
+    public String getImmatriculation() {
+        return immatriculation;
+    }
+
+    public void setImmatriculation(String immatriculation) {
+        this.immatriculation = immatriculation;
+    }
+
+    public String getMarque() {
+        return marque;
+    }
+
+    public void setMarque(String marque) {
+        this.marque = marque;
+    }
+
+    public String getModele() {
+        return modele;
+    }
+
+    public void setModele(String modele) {
+        this.modele = modele;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+
+    public Uri getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(Uri photo) {
+        this.photo = photo;
+    }
+
+    public boolean isLocation() {
+        return location;
+    }
+
+    public void setLocation(boolean location) {
+        this.location = location;
+    }
+
+
+}
