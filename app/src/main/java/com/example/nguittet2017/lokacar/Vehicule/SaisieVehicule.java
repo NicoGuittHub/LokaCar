0bc86c2a7b45e12bdc904283fb4ec61b9542550c
Unknown <ducatnico@gmail.com>
2018-06-25 11:36:40 +0200

    Auto stash before merge of "master" and "origin/master"

--- app/src/main/java/com/example/nguittet2017/lokacar/Vehicule/SaisieVehicule.java
+++ app/src/main/java/com/example/nguittet2017/lokacar/Vehicule/SaisieVehicule.java
@@ -11,8 +11,9 @@ public class SaisieVehicule extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saisie_vehicule);
+
     }
 
 
-    
+
 }
